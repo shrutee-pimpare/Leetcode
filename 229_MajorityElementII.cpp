d49c1cb20cc3ea6ebe0639b5873a7559ea4fed67
Shrutee_Pimpare20 <shruteepimpare209@gmail.com>
2023-10-03 14:47:17 +0530

    Create 229_MajorityElementII.cpp

---
+++ 229_MajorityElementII.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> majorityElement(vector<int>& nums) {
+        int cnt1 = 0, cnt2 = 0;
+        int el1 = INT_MIN, el2 = INT_MIN;
+        int n = nums.size();
+        
+        for (int i = 0; i < n; i++) {
+            if (cnt1 == 0 && el2 != nums[i]) {
+                cnt1 = 1;
+                el1 = nums[i];
+            } else if (cnt2 == 0 && el1 != nums[i]) {
+                cnt2 = 1;
+                el2 = nums[i];
+            } else if (nums[i] == el1) {
+                cnt1++;
+            } else if (nums[i] == el2) {
+                cnt2++;
+            } else {
+                cnt1--;
+                cnt2--;
+            }
+        }
+        
+        cnt1 = 0;
+        cnt2 = 0;
+        
+        for (int i = 0; i < n; i++) {
+            if (el1 == nums[i]) {
+                cnt1++;
+            }
+            if (el2 == nums[i]) {
+                cnt2++;
+            }
+        }
+        
+        int mini = (int)(n / 3);
+        vector<int> res;
+        
+        if (cnt1 > mini) {
+            res.push_back(el1);
+        }
+        
+        if (cnt2 > mini) { 
+            res.push_back(el2);
+        }
+        
+        sort(res.begin(), res.end());
+
+        return res;
+    }
+};
